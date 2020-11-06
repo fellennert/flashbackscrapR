821a1fc3c15bfe9fc3ee9c120edcf2f619a35dd9
fellennert <felix.lennert@icloud.com>
2020-11-06 14:32:57 +0100

    fixed crucial typo

--- R/get_missing_threads.R
+++ R/get_missing_threads.R
@@ -4,31 +4,31 @@
 #' missing ones
 #'
 #' @param link_tbl A tibble containing the thread links that are to be scraped;
-#' thread links need to be stored in a column named thread_links
+#' thread links need to be stored in a column named \code{thread_links}
 #' @param folder_name A character vector with a folder name the scraped files
 #' are stored in
 #'
-#' @return A tibble with two columns: the sub(sub)section's suffix and a folder
-#' name based on the given folder name and the sub(sub)section's name
+#' @return The link tibble containing the links that haven't been scraped so far
 #'
 #' @examples
-#' get_full_section_subs(main_section_suffix = "/f102", folder_name = NULL)
+#' get_full_section_subs(link_tbl = links_science, folder_name = "science")
 #'
 #' @export
 get_missing_urls <- function(link_tbl, folder_name){
-  links_scraped <- fs::ir_tree("science") %>%
-  purrr::map(purrr::safely(~readr::read_csv(.x, col_types = readr::cols(
-    url = readr::col_character(),
-    date = readr::col_date(format = ""),
-    time = readr::col_time(format = ""),
-    author_name = readr::col_character(),
-    author_link = readr::col_character(),
-    quoted_user = readr::col_character(),
-    posting = readr::col_character(),
-    posting_wo_quote = readr::col_character()
-  ), n_max = 5))) %>%
-  purrr::map_dfr("result") %>%
-  dplyr::distinct(url)
+  links_scraped <- fs::dir_tree(folder_name) %>%
+    purrr::map(purrr::safely(~readr::read_csv(.x, col_types = readr::cols(
+      url = readr::col_character(),
+      date = readr::col_date(format = ""),
+      time = readr::col_time(format = ""),
+      author_name = readr::col_character(),
+      author_link = readr::col_character(),
+      quoted_user = readr::col_character(),
+      posting = readr::col_character(),
+      posting_wo_quote = readr::col_character()
+    ),
+    n_max = 5))) %>%
+    purrr::map_dfr("result") %>%
+    dplyr::distinct(url)
 
   link_tbl %>% dplyr::filter(!thread_links %in% links_scraped$url)
 }
