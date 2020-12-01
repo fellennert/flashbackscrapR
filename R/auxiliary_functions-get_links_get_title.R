542dd7939fd7fc6bf29f0cfcaf008cbc17e81ccd
fellennert <felix.lennert@icloud.com>
2020-12-01 13:57:12 +0100

    simplifications

--- R/auxiliary_functions-get_links_get_title.R
+++ R/auxiliary_functions-get_links_get_title.R
@@ -26,40 +26,18 @@ generate_links <- function(suffix, n_pages) {
 
 
 get_date_links <- function(page) {
-  date <- rvest::html_nodes(page, ".td_last_post div") %>%
-    rvest::html_text()
-  date <- stringr::str_extract_all(date, "[:digit:]{4}\\-[:digit:]{2}\\-[:digit:]{2}|Igår|Idag") %>%
-    unlist()
-  for (i in seq_along(date)) {
-    if (date[i] == "Idag") {
-      date[i] <- as.character(lubridate::today())
-    } else {
-      if (date[i] == "Igår") {
-        date[i] <- as.character(lubridate::today() - 1)
-      } else {
-        date[i] <- date[i]
-      }
-    }
-  }
-  return(lubridate::ymd(date))
+  rvest::html_nodes(page, ".td_last_post div") %>%
+    rvest::html_text() %>%
+    stringr::str_extract("[:digit:]{4}\\-[:digit:]{2}\\-[:digit:]{2}|Igår|Idag") %>%
+    .[!is.na(.)] %>%
+    stringr::str_replace_all(pattern = c("Igår" = as.character(lubridate::today() - 1),
+                                         "Idag" = as.character(lubridate::today())))
 }
 
 
 get_links <- function(page) {
-  threads <- rvest::html_nodes(page,
-                        xpath="//td[contains(@class, 'td_title')]") %>%
-    rvest::html_nodes("a") %>%
+  rvest::html_nodes(page, ".pull-right+ a") %>%
     rvest::html_attr("href")
-  threads <- threads[stringr::str_detect(threads, "/t")]
-  threads <- stringr::str_split(threads, "l")
-  threads <- purrr::map_chr(threads, 1)
-  threads <- stringr::str_split(threads, "p")
-  threads <- purrr::map_chr(threads, 1)
-  threads <- stringr::str_split(threads, "s")
-  threads <- purrr::map_chr(threads, 1)
-  threads <- unique(threads)
-
-  return(threads)
 }
 
 
@@ -72,13 +50,8 @@ check_flyttad <- function(page) {
 
 
 get_thread_title <- function(page) {
-  threads <- rvest::html_nodes(page,
-                        xpath="//td[contains(@class, 'td_title')]") %>%
+  rvest::html_nodes(page, ".pull-right+ a") %>%
     rvest::html_text()
-  threads <- threads[stringr::str_detect(threads, "\\d+.visningar")]
-  thread_name <- stringr::str_extract(threads, "\\t[[:print:]]+\\n")
-  thread_name <- stringr::str_replace_all(thread_name, "\\t|\\n", "")
-  return(thread_name)
 }
 
 
